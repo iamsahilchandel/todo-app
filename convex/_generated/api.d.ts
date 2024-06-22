c3691d96d858c7ea041d4e0694e9c4b9e5ee2155
Sahil Chandel <rajputsahilchandel@gmail.com>
2024-06-22 16:23:13 +0530

    Config auth in Convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as auth from "../auth.js";
 import type * as authAdapter from "../authAdapter.js";
 import type * as http from "../http.js";
 import type * as tasks from "../tasks.js";
@@ -27,6 +28,7 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
   authAdapter: typeof authAdapter;
   http: typeof http;
   tasks: typeof tasks;
