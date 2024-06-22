576296bec760dba6ac91e05b7e6249fec8872d36
Sahil Chandel <rajputsahilchandel@gmail.com>
2024-06-22 13:02:06 +0530

    Added basic convex setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ConvexClientProvider from "./convex-client-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "TodoVex",
-  description: "TodoVex seamlessly organises your tasks and predicts what's nextusing AI.", 
+  description:
+    "TodoVex seamlessly organises your tasks and predicts what's nextusing AI.",
 };
 
 export default function RootLayout({
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
