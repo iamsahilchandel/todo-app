28df266bc01fb2b937f76b0169ede5237abe367b
Sahil Chandel <rajputsahilchandel@gmail.com>
2024-06-22 12:35:16 +0530

    Added button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Todo App</h1>
+      <Button variant={"destructive"}>Click Me!</Button>
     </main>
   );
 }
