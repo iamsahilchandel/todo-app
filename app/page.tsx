3628cc1a1cc0916a6e055e39725e15986c0f59d7
Sahil Chandel <rajputsahilchandel@gmail.com>
2024-06-22 14:10:40 +0530

    updated home page with sign in form

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-gradient-to-r from-purple-200 to-orange-200 h-full min-h-screen">
       <h1>Todo App</h1>
       <Button variant={"destructive"}>Click Me!</Button>
 
       <Tasks />
+      <form action={signInAction}>
+        <Button variant={"outline"}>Sign In</Button>
+      </form>
     </main>
   );
 }
